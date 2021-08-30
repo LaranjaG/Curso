eb98b1647ac349c366c06786958fae22d37924b0
Rayslla <68258208+LaranjaG@users.noreply.github.com>
2021-08-30 01:16:03 -0300

    Add files via upload

---
+++ SQL/Funções e selects.sql
@@ -0,0 +1,302 @@
+﻿-- Questão 1
+ -- função
+CREATE OR REPLACE FUNCTION recalcular()
+RETURNS trigger AS
+$BODY$
+
+BEGIN
+       IF (TG_TABLE_NAME = 'nota') THEN
+         UPDATE matricula ma SET nota_media = 
+         (select avg(valor) from nota where fk_matricula = ma.pk_matricula);
+       END IF;
+   
+       IF (TG_TABLE_NAME = 'frequencia') THEN
+         UPDATE matricula ma SET frequencia_total = 
+         (select avg(valor) from frequencia where fk_matricula = ma.pk_matricula);
+       END IF;
+  RETURN null;
+END
+$BODY$
+  LANGUAGE plpgsql;
+------------------------------------------------------------------------------------
+-- Gatinho nota
+Create trigger atualizar_nota
+  after Insert or Update or Delete
+  On nota
+  For Each Row
+  Execute procedure recalcular();
+------------------------------------------------
+-- Gatinho frequecia
+Create trigger atualizar_frequencia
+  after Insert or Update or Delete
+  On frequencia
+  For Each Row
+  Execute procedure recalcular();  
+  
+-----------------------------------------------------------------------------------------------------------------------------------
+
+-- Questão 3
+-- letra a
+  Select * from aluno 
+  Where sexo = 'm';
+  
+------------------------------------------------
+-- letra b
+  Select nome, nascimento from aluno 
+  Where sexo = 'f';
+  
+------------------------------------------------
+-- letra c
+  Select * from aluno a
+  Join matricula m on a.pk_aluno = m.fk_aluno
+  Join disciplina d on d.pk_disciplina = m.fk_disciplina
+  Where extract ("year" from now()) - extract ("year"
+  from a.nascimento) > 16 and d.nome = 'Lógica';
+  
+------------------------------------------------
+-- letra d
+  Select nome from aluno 
+  Where cpf 
+  In (Select cpf from funcionario);
+  
+------------------------------------------------
+-- letra e
+  Select nome, cpf from funcionario 
+  Where cargo = 'professor';
+  
+------------------------------------------------
+-- letra f
+  Select nome, cpf from funcionario 
+  Where pk_funcionario 
+  In (select fk_professor_coordenador from curso); 
+  
+------------------------------------------------
+-- letra g
+  Select f.nome, d.nome, periodo, turno from funcionario f 
+  Join disciplina d on f.pk_funcionario = d.fk_professor;
+  
+------------------------------------------------
+-- letra h - (tamo sem curso de Agro);
+  Select * from disciplina 
+  Where fk_curso 
+  In (Select pk_curso from  curso where nome = 'ALI');
+  
+------------------------------------------------
+-- letra i
+  Select a.* from aluno a 
+  Join matricula m on a.pk_aluno = m.fk_aluno
+  Join disciplina d on d.pk_disciplina = m.fk_disciplina
+  Where turno = 'Matutino';
+  
+------------------------------------------------
+-- letra j
+  Select distinct a.nome from aluno a
+  Join curso c on c.nome = 'TSI'  
+  Join disciplina d on d.fk_curso = pk_curso and d.turno = 'Noturno'
+  Join matricula m  on m.fk_disciplina = d.pk_disciplina 
+  Where d.periodo = '3º' and fk_aluno = pk_aluno;
+  
+------------------------------------------------
+-- letra k
+  Select nome, ano_criacao from curso
+  Where extract ("year" from now()) - ano_criacao > 10;
+  
+------------------------------------------------
+-- letra l - tamo sem curso de agro
+  Select distinct f.nome, f.rg, f.cpf from funcionario f
+  Join disciplina d on fk_professor = pk_funcionario
+  Join curso c      on fk_curso = pk_curso
+  Where c.nome = 'TSI';
+  
+------------------------------------------------
+-- letra m - tamo sem Fundamentos da Informática
+  Select a.nome, a.nascimento from aluno a 
+  Join disciplina d on d.nome = 'POO'
+  Join matricula m  on pk_aluno = fk_aluno and fk_disciplina = pk_disciplina;
+  
+------------------------------------------------
+-- letra n - tem curso de ALI, mas tamo se Dependencias nele...
+  Select a.nome, d.nome from aluno a
+  Join curso c on c.nome = 'BCC'
+  Join disciplina d on fk_curso = pk_curso
+  Join matricula m  on m.fk_disciplina = d.pk_disciplina 
+  Where m.dependencia and m.fk_aluno = a.pk_aluno;
+  
+------------------------------------------------
+-- letra o -  tamo sem curso de manutenção
+  Select distinct a.nome, d.nome, f.valor from aluno a
+  Join curso c      on c.nome = 'BCC'  
+  Join disciplina d on d.fk_curso = pk_curso
+  Join matricula m  on m.fk_disciplina = pk_disciplina
+  Join frequencia f on fk_matricula = pk_matricula
+  Where pk_aluno = fk_aluno;
+  
+------------------------------------------------
+-- letra p - ninguem tirou nota menor q 3, stonks
+  Select distinct a.nome, d.nome from aluno a
+  Join matricula m  on fk_aluno = pk_aluno
+  Join disciplina d on fk_disciplina = pk_disciplina
+  Join nota n       on pk_matricula = fk_matricula 
+  Where n.valor < 3;
+  
+------------------------------------------------
+-- letra q - eu acho q a frequencia ta em porcentagem, mas n é uma certeza...
+  Select distinct a.nome, d.nome from aluno a
+  Join matricula m  on fk_aluno = pk_aluno
+  Join disciplina d on fk_disciplina = pk_disciplina
+  Join nota n       on m.pk_matricula = fk_matricula
+  Join frequencia f on pk_matricula = f.fk_matricula
+  Where f.valor > 70 and n.valor < 4;
+  
+------------------------------------------------
+-- letra r - tamo sem a disciplina do enunciado
+  Select a.nome, c.nome from aluno a
+  Join matricula m  on M.fk_aluno = a.pk_aluno
+  Join disciplina d on d.pk_disciplina =  m.fk_disciplina
+  Join curso c on pk_curso = fk_curso -- temos pessoas com mesmo nome só que em cursos diferentes (podem ser a msm pessoa, por falta de pessoal...)
+  Where d.nome = 'POO' and sexo = 'm'
+  And pk_aluno not in 
+  (Select pk_aluno from aluno a
+  Join matricula m  on m.fk_aluno = a.pk_aluno
+  Join disciplina d on d.pk_disciplina =  m.fk_disciplina
+  Where d.nome = 'BD'); 
+
+------------------------------------------------
+-- letra s - tamo sem pessoal pra dar aula, por isso o resultado com essa base de dados volta vazio
+  Select distinct f.nome from funcionario f
+  Join disciplina d on fk_professor = pk_funcionario 
+  Join curso c      on fk_curso = pk_curso
+  Where c.nome = 'BCC' 
+  And fk_professor not in
+  (Select fk_professor from funcionario f
+  Join disciplina d on fk_professor = pk_funcionario 
+  Join curso c      on fk_curso = pk_curso
+  Where c.nome = 'TSI');
+
+------------------------------------------------
+-- letra t
+  Select distinct f.nome, count(pk_aluno) as num_alunos_dependencia from funcionario f
+  Join matricula m  on dependencia
+  Join aluno a on pk_aluno = fk_aluno
+  Join disciplina d on pk_disciplina = m.fk_disciplina 
+  Where fk_professor = pk_funcionario
+  Group by f.nome;
+
+------------------------------------------------
+-- letra u
+  Select d.nome, avg(nota_media) from matricula
+  Join disciplina d on pk_disciplina = fk_disciplina
+  Where d.nome = 'POO'
+  Group by d.nome;
+
+------------------------------------------------
+-- letra v - tamo sem curso de desenvolvimento
+  Select c.nome, Max(valor), Min(valor) from frequencia f
+  Join matricula m  on pk_matricula = f.fk_matricula
+  Join disciplina d on pk_disciplina = m.fk_disciplina 
+  Join curso c      on pk_curso = fk_curso
+  Where c.nome = 'ALI'
+  Group by c.nome;
+
+------------------------------------------------
+-- letra w
+  Select a.nome from aluno a 
+  Join funcionario f on a.cpf = f.cpf
+  Where f.cpf not in (Select cpf from funcionario Where cargo = 'professor');
+
+------------------------------------------------
+-- letra x
+  Select nome from curso 
+  Where pk_curso 
+  Not in
+  (Select distinct pk_curso from curso c, funcionario f
+  Join aluno a     on a.cpf = f.cpf
+  Join matricula m on fk_aluno = pk_aluno
+  Join disciplina  on fk_disciplina = pk_disciplina
+  Where pk_curso = fk_curso); 
+
+------------------------------------------------
+-- letra y
+  Select distinct a.nome as alunos, d.nome as disciplinas, d.periodo, d.turno, nota_media as nota_media, f.valor as frequencia from aluno a
+  Join curso c on c.nome = 'TSI'
+  Join disciplina d on fk_curso = pk_curso
+  Join matricula m  on fk_disciplina = pk_disciplina 
+  Join frequencia f on f.fk_matricula = pk_matricula
+  Where fk_aluno = pk_aluno;
+
+------------------------------------------------
+-- letra z
+  Select d.nome,max(n.valor),min(n.valor),avg(nota_media) media  from matricula m   
+  join disciplina d on d.pk_disciplina = m.fk_disciplina
+  join nota n on n.fk_matricula = m.pk_matricula
+  Group by d.nome;
+  
+------------------------------------------------
+-----------------------------------------------------------------------------------------------------------------------------------
+
+-- Questão 4
+-- letra a
+  Create view Numero_alunos_dependencia_cada_curso as 
+    Select c.nome as curso, Count(dependencia) as N_alunos_dependencia from curso c
+    Join disciplina on pk_curso = fk_curso
+    Join matricula on pk_disciplina = fk_disciplina
+    Where dependencia
+    Group by c.nome;
+    
+------------------------------------------------
+-- letra b
+  Create view Disciplinas_quantidade_alunos as
+    Select d.nome as disciplina, periodo, turno , Count(fk_disciplina) from  disciplina d
+    Join matricula on fk_disciplina = pk_disciplina
+    Group by disciplina, periodo, turno;
+
+------------------------------------------------
+-- letra c
+  Create view Professores_lecionam_mas_3_disciplinas as
+    Select f.nome, count(*) as Disciplinas_lecionadas  from funcionario f
+    Join curso c on fk_professor_coordenador = pk_funcionario
+    Join disciplina on fk_professor = pk_funcionario
+    Group by f.nome
+    Having count(*) > 3;
+
+------------------------------------------------
+-- letra d
+Create view Professores_coor_mediadisciplinas_menor6 as
+    Select distinct f.nome as funcionario, d.nome as disciplina, avg(nota_media) as nota from funcionario f, curso c   
+    Join disciplina d on fk_professor = fk_professor_coordenador
+    Join matricula m  on fk_disciplina = pk_disciplina
+    Join nota n on fk_matricula = pk_matricula
+    Where pk_funcionario = fk_professor_coordenador
+    Group by f.nome, d.nome 
+    Having avg(nota_media) < 6;
+
+------------------------------------------------
+-- letra e
+    Create view faltas_cada_disciplina_aluno as 
+    Select distinct a.nome as aluno, d.nome as disciplina, (carga_horaria - frequencia_total) as qtd_faltas from disciplina d
+    Join matricula m  on fk_disciplina = pk_disciplina
+    Join nota n on fk_matricula = pk_matricula
+    Join aluno a on fk_aluno = pk_aluno; 
+
+-----------------------------------------------------------------------------------------------------------------------------------
+    
+-- Questão 5
+-- letra a
+    Select a.nome as aluno, d.nome as disciplina, nota_media, avg(valor) as media from matricula 
+    Join nota on fk_matricula = pk_matricula
+    Join aluno a on pk_aluno = fk_aluno
+    Join disciplina d on pk_disciplina = fk_disciplina
+    Group by aluno, disciplina, nota_media
+    Having avg(valor) <> nota_media;
+    
+------------------------------------------------
+-- letra b
+    Select a.nome as aluno, d.nome as disciplina, frequencia_total, sum(valor) as total, carga_horaria from matricula 
+    Join frequencia on fk_matricula = pk_matricula
+    Join aluno a on pk_aluno = fk_aluno
+    Join disciplina d on pk_disciplina = fk_disciplina
+    Group by aluno, disciplina, frequencia_total, carga_horaria
+    Having sum(valor) <> frequencia_total and frequencia_total <> carga_horaria;
+
+------------------------------------------------
+-----------------------------------------------------------------------------------------------------------------------------------
---
+++ SQL/Trigger, funções e tabelas.sql
@@ -0,0 +1,280 @@
+﻿Drop table aluno;
+
+Create table aluno(
+pk_aluno serial primary key,
+nome varchar(50) NOT NULL,
+nascimento date,
+nome_pai varchar(60),
+nome_mae varchar(50) NOT NULL,
+cpf char(14) UNIQUE NOT NULL,
+rg varchar(10) UNIQUE NOT NULL,
+sexo char(1) CHECK(sexo='m' or sexo ='f'),
+CONSTRAINT UNIQUE_NOMES UNIQUE (nome, nome_pai, nome_mae)
+);
+	
+insert into aluno (nome, nascimento, nome_pai, nome_mae, cpf, rg, sexo) 
+	values('Fulani', '16/04/2001', 'clau', 'nana', '70540', '001', 'f'),
+	      ('Beutani', '02/08/1998', 'vlau', 'nene', '12345', '112', 'm'),
+	      ('Fulani Jorge', '29/11/2001', 'clau', 'nana', '98787', '223', 'f'),
+	      ('Fulani breutrani', '25/12/2000', 'cacw', 'veve', '77741', '334', 'm'),
+	      ('Betrani da Silva Camargo', '26/05/2002', 'bii', 'gigi', '7054', '6405', 'f');
+
+select * from aluno;
+
+-----------------------------------------------------------------------------------------------------------------------------------
+
+Drop table funcionario;
+
+Create table funcionario(
+pk_funcionario serial primary key,
+nome varchar(60) NOT NULL, 
+nascimento date, 
+cpf char(14) UNIQUE NOT NULL, 
+rg char(10) UNIQUE NOT NULL, 
+sexo char(1) check(sexo = 'm' or sexo ='f'), 
+cargo varchar(60)
+);
+
+insert into funcionario (nome, nascimento, cpf, rg, sexo, cargo)
+		  values('Beutani', '02/08/1998', '12345', '112', 'm', 'professor'),
+			('Vlausdinei', '03/12/1975', '55555', '445', 'm', 'diretor'),
+			('Vaniamirian', '14/01/1988', '66459', '556', 'f', 'professor'),
+			('Catacoquim da Silva', '26/02/1500', '77889', '667', 'f', 'professor'),
+			('Vandiscreia', '28/02/1978', '99956', '778', 'f', 'TI');
+
+select * from funcionario;
+
+-----------------------------------------------------------------------------------------------------------------------------------
+
+Drop table curso;
+
+Create table curso(
+pk_curso serial primary key, 
+nome varchar(60) NOT NULL, 
+fk_professor_coordenador int, 
+ano_criacao int,
+foreign key (fk_professor_coordenador) References funcionario(pk_funcionario)  
+on delete set null
+);
+
+insert into curso(nome, fk_professor_coordenador,ano_criacao)
+	   values('TSI', 1, 2009),
+		 ('ALI', 3, 2010),
+		 ('BCC', 4, 2017);
+		 
+select * from curso;
+
+------------------------------------------------------------------------------------
+--Função para checar se o coordenador é professor
+	CREATE OR REPLACE FUNCTION professor()
+	RETURNS trigger AS
+	$BODY$
+	BEGIN
+	
+	  IF NEW.fk_professor_coordenador in (Select pk_funcionario from funcionario Where cargo <> 'professor') THEN
+	      RAISE EXCEPTION 'Funcionario que não é professor encontrado na lista de inserção'; 
+	      RETURN NULL;
+	  END IF;
+	  
+	  Return NEW;  
+	 
+	END
+	$BODY$
+	  LANGUAGE plpgsql;
+	  
+------------------------------------------------------------------------------------
+
+-- Gatinho nota
+Create trigger professor
+  before Insert or Update
+  On curso
+  For Each Row
+  Execute procedure professor();
+
+-----------------------------------------------------------------------------------------------------------------------------------
+
+Drop table disciplina;
+
+Create table disciplina(
+	pk_disciplina serial primary key,
+	nome varchar(60) NOT NULL,
+	periodo varchar(15) ,
+	turno varchar(15),
+	fk_professor int,
+	fk_curso int,
+	carga_horaria float,
+	foreign key (fk_professor) references funcionario(pk_funcionario),
+	foreign key (fk_curso) references curso(pk_curso),
+	CONSTRAINT unique_disciplina UNIQUE(nome, periodo, turno),
+	CONSTRAINT unique_disciplina_curso UNIQUE(nome, periodo, turno, fk_curso)
+	);
+
+Insert into disciplina
+	(nome, periodo, turno, fk_professor, fk_curso, carga_horaria)
+Values
+	('Lógica', '1º', 'Noturno', 1, 1, 30.2),
+	('POO', '3º','Noturno', 3, 1, 62.8),
+	('Projeto de banco de dados', '2º', 'Noturno', 4, 1, 30),
+        ('Desenho tecnico', '3º', 'Noturno', 1, 2, 32),
+        ('Embalagens', '4º', 'Noturno', 3, 2, 33),
+        ('Leite', '3º', 'Noturno', 4, 2, 45),
+        ('BD', '3º', 'vespertino', 1, 3, 27),
+        ('POO', '4º', 'Matutino', 3, 3, 72),
+        ('MSOO', '3º', 'Vespertino', 4, 3, 31.5);
+        
+Select * from disciplina;
+
+-----------------------------------------------------------------------------------------------------------------------------------
+
+Drop table matricula;
+
+Create table matricula(
+	pk_matricula serial primary key,
+	fk_aluno int,
+	fk_disciplina int,
+	dependencia boolean,
+	nota_media float,
+	frequencia_total float,
+	foreign key (fk_aluno) references aluno(pk_aluno)
+	on delete set null on update cascade,
+	foreign key (fk_disciplina) references disciplina(pk_disciplina)
+	on delete set null on update cascade
+);
+
+
+
+Insert into matricula
+	(fk_aluno,fk_disciplina,dependencia, nota_media, frequencia_total)
+Values
+	(1,1,'FALSE', 6, 0),
+	(2,1,'FALSE', 6.5, 0),
+	(2,2, 'FALSE', 7, 0),
+	(3,2, 'FALSE', 8, 0),
+	(3,3, 'TRUE', 5.5, 0),
+	(4,3, 'TRUE', 4.7, 0),
+	(4,4, 'FALSE', 10, 0),
+	(5,4, 'FALSE', 9.5, 0),
+	(1,5, 'FALSE', 8, 0),
+	(5,5, 'FALSE', 7.5, 0),
+	(3,6, 'FALSE', 7, 0),
+	(2,6, 'FALSE', 8, 0),
+	(4,7, 'TRUE', 5.5, 0),
+	(3,7, 'TRUE', 4.7, 0),
+	(1,8, 'FALSE', 10, 0),
+	(2,8, 'FALSE', 9.5, 0),
+	(3,9, 'FALSE', 8, 0),
+	(5,9, 'FALSE', 7.5, 0);
+
+select * from matricula;
+
+-----------------------------------------------------------------------------------------------------------------------------------
+
+Drop table nota;
+
+Create table nota(
+	pk_nota serial primary key,
+	fk_matricula int NOT NULL,
+	valor decimal(10,2) NOT NULL,
+	foreign key (fk_matricula) references matricula(pk_matricula)
+	on delete cascade	
+);
+
+Insert into nota
+	(fk_matricula, valor)
+Values
+	(1, 10),
+	(1, 5),
+	(2, 7),
+	(2, 6),
+	(3, 8),
+	(3, 6),
+	(4, 9.5),
+	(4, 6.5),
+	(5, 7),
+	(5, 4),
+	(6, 5.5),
+	(6, 3.9),
+	(7, 10),
+	(7, 10),
+	(8, 9),
+	(8, 10),
+	(9, 7),
+	(9, 9),
+	(10, 7),
+	(10, 8),
+	(11, 7),
+	(11, 7),
+	(12, 9),
+	(12, 6),
+	(13, 4),
+	(13, 7),
+	(14, 5.4),
+	(14, 4),
+	(15, 10),
+	(15, 10),
+	(16, 9),
+	(16, 10),
+	(17, 9),
+	(17, 7),
+	(18, 7),
+	(18, 8);
+
+Select * from nota;
+
+-----------------------------------------------------------------------------------------------------------------------------------
+
+Drop table frequencia;
+
+Create table frequencia(
+	pk_frequencia serial primary key,
+	fk_matricula int NOT NULL,
+	valor float NOT NULL,
+	dia date,
+	foreign key (fk_matricula) references matricula(pk_matricula)
+	on delete cascade
+);
+
+
+Insert into frequencia
+	(fk_matricula, valor, dia)
+Values
+	(1, 90, now()),
+	(1, 98, now()),
+	(2, 75, now()),
+	(2, 74, now()),
+	(3, 70, now()),
+	(3, 50, now()),
+	(4, 60, now()),
+	(4, 68, now()),
+	(5, 82, now()),
+	(5, 64, now()),
+	(6, 90, now()),
+	(6, 98, now()),
+	(7, 75, now()),
+	(7, 74, now()),
+	(8, 70, now()),
+	(8, 50, now()),
+	(9, 60, now()),
+	(9, 68, now()),
+	(10, 82, now()),
+	(10, 64, now()),
+	(11, 90, now()),
+	(11, 98, now()),
+	(12, 75, now()),
+	(12, 74, now()),
+	(13, 70, now()),
+	(13, 50, now()),
+	(14, 60, now()),
+	(14, 68, now()),
+	(15, 82, now()),
+	(15, 64, now()),
+	(16, 90, now()),
+	(16, 98, now()),
+	(17, 75, now()),
+	(17, 74, now()),
+	(18, 70, now()),
+	(18, 50, now());
+
+Select * from frequencia;
+
+-----------------------------------------------------------------------------------------------------------------------------------
