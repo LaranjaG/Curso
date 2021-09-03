47e7a068f4be1ad18c801c6e3feb252585e4a168
Rayslla <68258208+LaranjaG@users.noreply.github.com>
2021-09-03 01:12:01 -0300

    Add files via upload

---
+++ C#/OO/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Banco banco = new Banco("Nu");
+            banco.Nome += "Bank";
+            Conta conta = new Conta("Rayslla", 19.99, "Corrente");
+            banco.addConta = conta;
+            System.Console.WriteLine($"name banco: {banco.Nome}");
+            
+            foreach (Conta ct in banco.Contas)
+            {
+                System.Console.WriteLine($"Nome user: {ct.Nome}, saldo: {ct.Saldo}, tipo conta: {ct.TipoConta}");
+            }
+        }
+    }
+}
